98a5ea468fc1b9f8f3cf0acf4462056629987561
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-07-17 17:31:04 +0800

    feat: add vueCompilerOptions into tsconfig

--- src/types/tsconfig.ts
+++ src/types/tsconfig.ts
@@ -1,4 +1,5 @@
 import type { CompilerOptions, TypeAcquisition } from "typescript";
+import type { VueCompilerOptions } from "@vue/language-core";
 
 export type StripEnums<T extends Record<string, any>> = {
   [K in keyof T]: T[K] extends boolean
@@ -16,6 +17,7 @@ export type StripEnums<T extends Record<string, any>> = {
 
 export interface TSConfig {
   compilerOptions?: StripEnums<CompilerOptions>;
+  vueCompilerOptions?: Partial<VueCompilerOptions>;
   exclude?: string[];
   compileOnSave?: boolean;
   extends?: string;
