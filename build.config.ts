29e54beabc3e3861abc60975e2d5819ee13b2fbe
Pooya Parsa <pooya@pi0.io>
2026-02-16 13:53:21 +0100

    build: migrate to obuild

--- build.config.ts
+++ build.config.ts
@@ -1,11 +1,5 @@
-import { defineBuildConfig } from "unbuild";
-import { rm } from "node:fs/promises";
+import { defineBuildConfig } from "obuild/config";
 
 export default defineBuildConfig({
-  externals: ["typescript"],
-  hooks: {
-    async "build:done"() {
-      await rm("dist/index.d.ts");
-    },
-  },
+  entries: ["src/index.ts"],
 });
