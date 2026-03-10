90103d015c7e8c9a738e06c1e854ef2f0ca0a26c
Daniel Roe <daniel@roe.dev>
2026-03-10 12:28:59 +0100

    refactor: use namespace import

--- build.config.ts
+++ build.config.ts
@@ -1,8 +1,5 @@
-import { defineBuildConfig } from "obuild/config"
+import { defineBuildConfig } from "obuild/config";
 
 export default defineBuildConfig({
-  externals: [
-    "#typescript"
-  ],
   entries: ["src/index.ts"],
 });
--- typescript.d.mts
+++
@@ -1,3 +0,0 @@
-import type ts from "typescript";
-export type CompilerOptions = ts.CompilerOptions;
-export type TypeAcquisition = ts.TypeAcquisition;
